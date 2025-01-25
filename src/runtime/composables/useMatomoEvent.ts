96d7b17ee922266e0a3dddd55b941d951fcd3edf
Ronan <ronan56.hello@gmail.com>
2025-01-25 16:11:38 +0100

    update: MatomoEvent composable

--- src/runtime/composables/useMatomoEvent.ts
+++ src/runtime/composables/useMatomoEvent.ts
@@ -1,4 +1,4 @@
-export default function useMatomoEvent(eventCategory, eventAction, eventName, eventValue = undefined, customDimensions = {}) {
+export default function useMatomoEvent(eventCategory = '', eventAction = '', eventName = '', eventValue = undefined, customDimensions = {}) {
   if (import.meta.client) {
     window._paq = window._paq || []
     window._paq.push(['trackEvent', eventCategory, eventAction, eventName, eventValue, customDimensions])
